1bbcd92fc82fee7f12434bf549afd2e2194118a9
Rafał Wasiluk <rafal.wasiluk@student.wat.edu.pl>
2021-01-22 00:19:55 +0100

    Dodane połączenie bazy danych, model bazy, utworzenie szablonu strony z trasami.

---
+++ Migrations/20210121231113_AddTrace.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Projekt.NET_CORE.Migrations
+{
+    public partial class AddTrace : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trace",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TraceName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TraceStart = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TraceEnd = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TracePoints = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trace", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trace");
+        }
+    }
+}
---
+++ Model/Trace.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt.NET_CORE.wwwroot.Model
+{
+    public class Trace
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string TraceName { get; set; }
+
+        [Required]
+        public string TraceStart { get; set; }
+
+        [Required]
+        public string TraceEnd { get; set; }
+
+        [Required]
+        public string Date { get; set; }
+
+        [Required]
+        public string TracePoints { get; set; }
+    }
+}
